74ee7899746f9c6c5dd616c7ece708b353cf5034
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-28 11:00:22 -0300

    feat(utils): adds formatProceWithoutDecimals

---
+++ src/utils/formatPriceWithoutDecimals.ts
@@ -0,0 +1,7 @@
+export function formatPriceWithoutDecimals(price: number) {
+  return price.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+  })
+}
