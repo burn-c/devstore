1628b7e61f9073699b3c57815e96812320bf0b54
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-30 19:13:52 -0300

    feat(Store): adds OpenGraph Image on Metadata pages

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,80 @@
+import { ImageResponse } from 'next/og'
+import colors from 'tailwindcss/colors'
+import { Product } from '@/data/@types/products'
+import { api } from '@/data/api'
+import { env } from '@/env'
+import Image from 'next/image'
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const alt = 'About Acme'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+interface ImageProps {
+  params: {
+    slug: string
+  }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const product = await response.json()
+
+  return product
+}
+
+// Image generation
+export default async function OgImage({ params }: ImageProps) {
+  const product = await getProduct(params.slug)
+
+  const productImageURL = new URL(product.image, env.APP_URL).toString()
+
+  // Custom Font
+  // const interSemiBold = fetch(
+  //   new URL('./Inter-SemiBold.ttf', import.meta.url),
+  // ).then((res) => res.arrayBuffer())
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          background: colors.zinc[950],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={productImageURL} alt="" style={{ width: '100%' }} />
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      //   fonts: [
+      //     {
+      //       name: 'Inter',
+      //       data: await interSemiBold,
+      //       style: 'normal',
+      //       weight: 400,
+      //     },
+      //   ],
+    },
+  )
+}
