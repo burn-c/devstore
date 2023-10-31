6398d1d0d9721b982fc5459fff7f4488efad1d24
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-31 09:34:52 -0300

    feat(comp): adds AddToCartButton with integration to CartContext

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { Product } from '@/data/@types/products'
 import { api } from '@/data/api'
 import { formatPrice } from '@/utils/priceFormatters'
+import AddToCartButton from '@/components/AddToCartButton'
 
 interface ProductProps {
   params: {
@@ -105,12 +106,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
