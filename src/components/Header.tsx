c364f56d15058b05f3ad19fc515ac271f4d44ed0
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-31 09:34:08 -0300

    feat(comp): adds CartWidget with integration to CartContext

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
-import { Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
+import { Search } from 'lucide-react'
+
+import CartWidget from './CartWidget'
 
 export default function Header() {
   return (
@@ -25,10 +27,7 @@ export default function Header() {
       </div>
 
       <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
-          <ShoppingBag className="h-4 w-4" />
-          <span className="text-sm">Cart (0)</span>
-        </div>
+        <CartWidget />
 
         <div className="w-px h-4 bg-zinc-700" />
 
